6983c7a3dbf9c0107fa8f6b6eccfe8d058975e27
anthony <diomede1991@libero.it>
2022-05-05 23:05:23 +0200

    dimenticato il push

--- laravel/database/migrations/2022_05_05_143041_create_train_table.php
+++ laravel/database/migrations/2022_05_05_143041_create_train_table.php
@@ -16,6 +16,16 @@ class CreateTrainTable extends Migration
         Schema::create('train', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('azienda',100);
+            $table->string('stazione_di_partenza' ,100);
+            $table->string('stazione_di_arrivo',100);
+            $table->time('orario_di_arrivo',0);
+            $table->time('orario_di_partenza',0);
+            $table->string('codice_treno',100);//non so se il treno ha anche lettere nel codice
+            $table->tinyInteger('numero_carozze');//treno con più carrozze al mondo 22
+            $table->boolean('in_orario');//presumo buleano si o no, 1 o 0
+            $table->boolean('cancellato');//buleano si o no
+            $table->string('scopo',100);//aggiunto, tipo trsporto merci o persone
         });
     }
 
